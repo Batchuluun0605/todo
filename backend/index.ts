5e614f717d62585caa6fd5f0d27843e9b28eed40
батчулуун цогбаяр <23LP2014@G303-CC0138.local>
2024-01-23 08:55:17 +0800

    a

--- backend/index.ts
+++ backend/index.ts
@@ -1,41 +1,40 @@
 // // primitive types
-// let firstname = "hello";
-// console.log(typeof firstname);
+let firstname = "hello";
+console.log(typeof firstname);
 
 // //special types type:any
-// let v: any;
-// v = "string";
-// v = true;
-// console.log(v);
+let v: any;
+v = "string";
+v = true;
+console.log(v);
 
 // // array
-// let arr: [];
-// arr = [];
-// console.log(arr);
+let arr: [];
+arr = [];
+console.log(arr);
 
 // // object types
-// let car: { type: string; model: string; year: number };
-// car = { type: "toyota", model: "corolla", year: 2009 };
-// console.log(car);
+let car: { type: string; model: string; year: number };
+car = { type: "toyota", model: "corolla", year: 2009 };
+console.log(car);
 
 // union types
-// function printStatusCode(code: string | number) {
-//   console.log(`My status code is ${code}.`);
-// }
-// printStatusCode(404);
-// printStatusCode("404");
+function printStatusCode(code: string | number) {
+  console.log(`My status code is ${code}.`);
+}
+printStatusCode(404);
+printStatusCode("404");
 
 // functions type
-// function printHello() : void {
-//   console.log("Hello!");
-// }
+function printHello(): void {
+  console.log("Hello!");
+}
 
-// printHello();
+printHello();
 
 // casting type
-// let x: unknown = "hello";
-
-// console.log((x as string).length);
+let x: unknown = "hello";
+console.log((x as string).length);
 type usersType = {
   name: string;
   age: number;
