259f919dee242f48b18aec42917b0c65bb3bd8dc
батчулуун цогбаяр <23LP2014@G303-CC0138.local>
2024-01-24 09:32:54 +0800

    a

--- backend/src/controller/user.ts
+++ backend/src/controller/user.ts
@@ -6,28 +6,43 @@ type SignUpType = {
   password: string;
   avatarImage: Buffer;
 };
-type LogInType = {
-  username: string;
-  password: string;
-  avatarImage: Buffer;
-};
+// type LogInType = {
+//   username: string;
+//   password: string;
+//   avatarImage: Buffer;
+// };
 export const signUp = async (req: Request, res: Response) => {
-  const { username, password }: Required<SignUpType> = req.body;
   try {
-    const result = await UserModel.create(username, password);
-    console.log(result);
-  } catch (error) {
-    return res.status(400).send({ success: true });
+    const { username, password }: Required<SignUpType> = req.body;
+    const saltRounds = 10;
+    bcrypt.hash(
+      password,
+      saltRounds,
+      async function (error: Error | undefined, hash: String) {
+        try {
+          const result = await UserModel.create({ username, password: hash });
+          console.log(result);
+        } catch (error) {
+          throw new Error(JSON.stringify(error));
+        }
+      }
+    );
+    return res.status(201).send({ success: true });
+  } catch (error: any) {
+    return res.status(400).send({ success: false, error: "Invalid request" });
   }
 };
 export const LogIn = async (req: Request, res: Response) => {
-  const { username, password }: Required<LogInType> = req.body;
+  const { username } = req.body;
   try {
     const result = await UserModel.findOne({
       username: username,
-      password: password,
     });
-    console.log(result);
+    if (result) {
+      console.log("success");
+    } else {
+      console.log("error");
+    }
     res.send("ok");
   } catch (error) {
     console.log(error);
